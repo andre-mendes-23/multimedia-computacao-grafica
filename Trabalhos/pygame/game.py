c6d0b1e6d94c1601218ecda58f69c044680090c3
André Mendes <125397738+andre-mendes-23@users.noreply.github.com>
2024-10-07 22:26:33 +0100

    Criação da v2 do jogo

---
+++ Trabalhos/pygame/game.py
@@ -0,0 +1,7 @@
+"""
+Nome do ficheiro: game.py
+Descrição: Ficheiro contendo a lógica do jogo.
+Criado em: 07/10/2024
+Autor: André Mendes
+"""
+
---
+++ Trabalhos/pygame/settings.py
@@ -0,0 +1,7 @@
+"""
+Nome do ficheiro: settings.py
+Descrição: Ficheiro contendo as configurações do jogo.
+Criado em: 07/10/2024
+Autor: André Mendes
+"""
+
